8ea08558156e1dcb30b860394194e722c99c72a4
Anubhab Guha <96384072+anubhabguha1999@users.noreply.github.com>
2023-02-02 00:40:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+//import About from './components/About';
+import React, {useState} from 'react'
+
 
 function App() {
+  const[mode,setMode]= useState('light');
+  const toggleMode = ()=>{
+    if(mode==='light'){
+      setMode ('dark')
+      document.body.style.backgroundColor="#042743";
+      
+    }
+    else{
+      setMode ('light')
+      document.body.style.backgroundColor="white";
+    }
+  }
   return (
     <>
-    <Navbar title = "Upper or Lower Case" />
+    <Navbar title = "Upper or Lower Case" mode ={mode} toggleMode ={toggleMode} />
     <div className="container m-3">
-    <TextForm heading ="Enter that you need to convert"/>
-    
+    <TextForm heading ="Enter that you need to convert"mode ={mode}/>
+    {/*<About/>*/}
     </div>
     </>
   );
